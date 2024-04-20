87464e778129619274fbb8ca336dc0b2c7ded7bb
Blaz333 <arseniyserebryakov17@gmail.com>
2024-04-20 15:47:39 +0200

    Add spring dependency

---
+++ src/main/java/com/arsenii/SpotifyNewReleasesApplication.java
@@ -0,0 +1,13 @@
+package com.arsenii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyNewReleasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyNewReleasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arsenii/SpotifyNewReleasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsenii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyNewReleasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
